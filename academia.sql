21a837425da491d623c93dbd47a2f0f00418e285
marcoMeirelles <marcomeirelles91@gmail.com>
2024-12-09 08:49:11 -0300

    Update academia.sql

--- academia.sql
+++ academia.sql
@@ -27,3 +27,40 @@ senha varchar(255) not null
 
 select * from usuario;
 ALTER TABLE usuario MODIFY COLUMN cpf VARCHAR(14);
+
+/*================sql atualizado================*/
+
+create database academia; 
+use academia; 
+
+create table usuario( 
+id int primary key auto_increment not null, 
+nome varchar(50) not null, email varchar(50) 
+not null, cpf char(11) not null, 
+senha varchar(255) not null );
+ ); 
+ 
+create table funcionario( 
+id int primary key auto_increment not null, 
+nome varchar(50) not null, 
+email varchar(50) not null, 
+cpf char(11) not null, 
+senha varchar(255) not null ); 
+
+create table adm( 
+id int primary key auto_increment not null,
+nome varchar(50) not null,
+email varchar(50) not null,
+cpf char(11) not null,
+senha varchar(255) not null );
+ 
+ 
+ CREATE TABLE recuperacao_senha ( 
+id INT AUTO_INCREMENT PRIMARY KEY,
+usuario_id INT NOT NULL,
+token VARCHAR(255) NOT NULL,
+expiracao TIMESTAMP NOT NULL,
+FOREIGN KEY (usuario_id) REFERENCES usuario(id) ); 
+
+
+
