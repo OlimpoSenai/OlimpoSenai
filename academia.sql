fe87b4c1f440584e094c6596d3d9098981c9a07b
marcoMeirelles <marcomeirelles91@gmail.com>
2024-12-10 13:52:54 -0300

    Add files via upload

--- academia.sql
+++ academia.sql
@@ -1,21 +1,22 @@
 create database academia; 
-use academia; 
+use academia;
 
 create table usuario( 
 id int primary key auto_increment not null, 
 nome varchar(50) not null, email varchar(50) 
 not null, cpf char(11) not null, 
-senha varchar(255) not null );
- ); 
+senha varchar(255) not null 
+);
  
-create table funcionario( 
+create table funcionario(
+ 
 id int primary key auto_increment not null, 
 nome varchar(50) not null, 
 email varchar(50) not null, 
 cpf char(11) not null, 
 senha varchar(255) not null ); 
 
-create table adm( 
+create table adm(
 id int primary key auto_increment not null,
 nome varchar(50) not null,
 email varchar(50) not null,
@@ -32,9 +33,20 @@ expiracao TIMESTAMP NOT NULL,
 FOREIGN KEY (usuario_id) REFERENCES usuario(id) 
 ); 
 
+CREATE TABLE treinos (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  usuario_id INT,
+  nome VARCHAR(255),
+  descricao TEXT,
+  data DATE,
+  FOREIGN KEY (usuario_id) REFERENCES usuario(id) -- assuming you have a 'usuario' table
+);
+
 select * from usuario;
+select * from funcionario;
 
-ALTER TABLE usuario MODIFY COLUMN cpf VARCHAR(14);
-delete * from usuario
-
+/*altere usando isso se der erro de cpf ou outro */
 ALTER TABLE usuario ADD COLUMN codigo_recuperacao VARCHAR(255);
+ALTER TABLE funcionario MODIFY COLUMN cpf VARCHAR(14);
+ALTER TABLE usuario MODIFY COLUMN cpf VARCHAR(14);
+
